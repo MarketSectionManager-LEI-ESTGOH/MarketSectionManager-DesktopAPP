afa7b421900d42f1fe2f903348552cb9210fa1f8
Bladed <fmr7@live.com>
2021-03-31 15:03:52 +0100

    Alterei a minha parte na Classe Main.

--- src/Controller/User.java
+++ src/Controller/User.java
@@ -66,4 +66,6 @@ public class User {
         return false;
     }
 
+
+
 }
--- src/View/Main.java
+++ src/View/Main.java
@@ -25,30 +25,8 @@ public class Main extends Application{
     }
 
     public static void main(String[] args)  {
-
-
-        //ISTO É APENAS UM TESTE AO FICHEIRO DAS PROPRIEDADES
-        ConnectDB c = new ConnectDB();
-
-        c.loadProperties();
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(c.getUrl(), c.getProperties());
-            PreparedStatement ps = conn.prepareStatement("SELECT nome FROM user WHERE nome = ?");
-            ps.clearParameters();
-            ps.setString(1, "John Doe");
-
-            if(c.checkString(ps, "John Doe")){
-                System.out.println("yes");
-            }else{
-                System.out.println("no");
-            }
             //Dá RUN AO FRAME
             launch(args);
-        } catch (Exception throwables) {
-            throwables.printStackTrace();
-        }
-
     }
 
 }
