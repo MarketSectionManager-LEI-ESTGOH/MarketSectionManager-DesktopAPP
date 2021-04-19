daed9c5c89b5d92b315410025486f5f6b18ccf01
João Duarte <71460038+jduarte98@users.noreply.github.com>
2021-04-19 14:40:00 +0100

    duno what is this but hey, its the situtaion

--- src/View/Main.java
+++ src/View/Main.java
@@ -11,7 +11,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-public class Main extends Application{
+public class Main extends Application {
 
     public static Controller.User u;
     public TextField funcNumber = new TextField();
@@ -20,9 +20,8 @@ public class Main extends Application{
     private Stage currentStage = null;
 
 
-
     @Override
-    public void start(Stage loginScreen) throws Exception{
+    public void start(Stage loginScreen) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
         loginScreen.setTitle("Market Section Manager");
         loginScreen.setScene(new Scene(root, 360, 275));
@@ -34,16 +33,17 @@ public class Main extends Application{
 
     /**
      * Main Function
+     *
      * @param args
      */
-    public static void main(String[] args)  {
-            launch(args);
+    public static void main(String[] args) {
+        launch(args);
     }
 
     /**
      * Apresenta uma cofirmação ao utilizador e se este clicar "Sim" o programa termina
      */
-    public void closeProgram(){
+    public void closeProgram() {
         Alert exitConfirmation = new Alert(Alert.AlertType.CONFIRMATION);
         exitConfirmation.setTitle("Confirmação de Saída");
         exitConfirmation.setHeaderText("Tem a certeza que pretende sair do programa?");
@@ -51,61 +51,64 @@ public class Main extends Application{
         ButtonType noBtn = new ButtonType("Não");
         exitConfirmation.getButtonTypes().setAll(yesBtn, noBtn);
         exitConfirmation.showAndWait();
-        if(exitConfirmation.getResult().equals(yesBtn)){
+        if (exitConfirmation.getResult().equals(yesBtn)) {
             System.exit(0);
         }
 
-    };
+    }
+
+    ;
 
     /**
      * Login
      */
-    public void login(){
+    public void login() {
         ConnectDB.loadProperties();
-        try{
+        try {
             int number = Integer.parseInt(funcNumber.getText());
-            if(Controller.User.checkLogin(number, funcPass.getText())){
+            if (Controller.User.checkLogin(number, funcPass.getText())) {
                 cleanFields();
                 currentStage = (Stage) loginBtn.getScene().getWindow();
 
                 //Criar novo objeto utilizador
                 u = User.createObjUser(number);
-                if(u.getUserType() == 1){
+                if (u.getUserType() == 1) {
                     cleanFields();
                     currentStage.close();
                     new MainScreenController().start();
-                }else {
-                    alerts(Alert.AlertType.ERROR,"Login Inválido","Este programa apenas permite o acesso a Administradores, pelo que a sua conta não tem permissão para fazer Login no mesmo!").showAndWait();
+                } else {
+                    alerts(Alert.AlertType.ERROR, "Login Inválido", "Este programa apenas permite o acesso a Administradores, pelo que a sua conta não tem permissão para fazer Login no mesmo!").showAndWait();
                     u = null;
                     cleanFields();
                 }
-            }else{
-                alerts(Alert.AlertType.ERROR,"Credenciais Erradas", "O Número de Funcionário ou a Password estão errados, por favor tente novamente!").showAndWait();
+            } else {
+                alerts(Alert.AlertType.ERROR, "Credenciais Erradas", "O Número de Funcionário ou a Password estão errados, por favor tente novamente!").showAndWait();
                 cleanFields();
             }
-        }catch(NumberFormatException nfe){
-            alerts(Alert.AlertType.ERROR,"ERRO", "O Campo Número de Funcionário apenas aceita números, por favor tente novamente!").showAndWait();
+        } catch (NumberFormatException nfe) {
+            alerts(Alert.AlertType.ERROR, "ERRO", "O Campo Número de Funcionário apenas aceita números, por favor tente novamente!").showAndWait();
             cleanFields();
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
-            alerts(Alert.AlertType.ERROR,"ERRO", "Aconteceu um erro inesperado, por favor tente novamente!").showAndWait();
+            alerts(Alert.AlertType.ERROR, "ERRO", "Aconteceu um erro inesperado, por favor tente novamente!").showAndWait();
             cleanFields();
         }
     }
 
-    private final void cleanFields(){
+    private final void cleanFields() {
         funcNumber.setText("");
         funcPass.setText("");
     }
 
     /**
      * Dá Alert PopUps
+     *
      * @param aAlertType Objeto Alert.AlertType, define o tipo de alerta (Erro, Info, ...)
-     * @param aTitle Titulo do PopUp
-     * @param aText Texto o PopUp
+     * @param aTitle     Titulo do PopUp
+     * @param aText      Texto o PopUp
      * @return Objeto do Tipo Alert
      */
-    private Alert alerts(Alert.AlertType aAlertType, String aTitle, String aText){
+    private Alert alerts(Alert.AlertType aAlertType, String aTitle, String aText) {
         Alert generalAlert = new Alert(aAlertType);
         generalAlert.setTitle(aTitle);
         generalAlert.setHeaderText("Aconteceu um Erro: ");
