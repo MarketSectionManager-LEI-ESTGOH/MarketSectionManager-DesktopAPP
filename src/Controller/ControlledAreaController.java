11f4735db909736ad41736324268c5704975953e
João Duarte <71460038+jduarte98@users.noreply.github.com>
2021-06-01 17:39:32 +0100

    MainScreenController finally cleaned up + AddControllers
    MainScreenController finally cleaned up + AddControllers

--- src/Controller/ControlledAreaController.java
+++ src/Controller/ControlledAreaController.java
@@ -20,7 +20,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Optional;
 
-public class SectionController {
+public class ControlledAreaController {
     @FXML
     private Pane areasControladasPane;
     @FXML
@@ -33,12 +33,8 @@ public class SectionController {
     public TextField searchAreasContTextField;
     public Button editAreaContBtn;
     public Button addAreasContBtn;
-    public Button addCAComponentsBTN;
-    public Button addCABTN;
-    @FXML
-    private TextField addCANumberTF;
-    @FXML
-    private TextField addCADesignTF;
+
+
     ObservableList<Area> listAreasCont;
     private int index = -1;
 
@@ -148,48 +144,5 @@ public class SectionController {
         }
     }
 
-    public void insertControlledArea(){
-        System.out.println(" -- @ insertControllerArea() --");
-        try{
-            int areaNumber = -1;
-            if(!addCANumberTF.getText().isEmpty()){
-                areaNumber = Integer.parseInt(addCANumberTF.getText());
-                if(!addCADesignTF.getText().isEmpty()){
-                    if(registerControlledArea(areaNumber, addCADesignTF.getText())){
-                        MainScreenController.alerts(Alert.AlertType.INFORMATION, "SUCESSO", " A Área Controlada " + addCADesignTF.getText() + " com o Número Interno " + areaNumber + "\nfoi inserida com sucesso!").showAndWait();
-                        cleanControlledAreaFields();
-                    }else{
-                        MainScreenController.alerts(Alert.AlertType.ERROR, "ERRO", "Aconteceu um erro inseperado, por favor tente novamente!").showAndWait();
-                    }
-                }else{
-                    System.out.println("design empty");
-                    MainScreenController.alerts(Alert.AlertType.ERROR, "ERRO", "A Designação da Área Controlada é de Preenchimento Obrigatório!").showAndWait();
-                }
-            }else{
-                System.out.println("number empty");
-                MainScreenController.alerts(Alert.AlertType.ERROR, "ERRO", "O número da Área Controlada é de Preenchimento Obrigatório!").showAndWait();
-            }
-        }catch(Exception e){
-            MainScreenController.alerts(Alert.AlertType.ERROR, "ERRO", "Algo correu mal, por favor tente novamente! \n\n " + e).showAndWait();
 
-        }
-    }
-
-    private void cleanControlledAreaFields(){
-        addCANumberTF.setText("");
-        addCADesignTF.setText("");
-    }
-
-    private boolean registerControlledArea(int aNumber, String aDesign){
-        try {
-            String stmt = "INSERT INTO area (numero, designacao) VALUES (?, ?)";
-            PreparedStatement ps = ConnectDB.getConn().prepareStatement(stmt);
-            ps.setInt(1, aNumber);
-            ps.setString(2,aDesign);
-            return ConnectDB.insertIntoTable(ps);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return false;
-    }
 }
