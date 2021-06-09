16b9be10cf2d1f6236aa24a5dd9b96edf958a12a
João Duarte <71460038+jduarte98@users.noreply.github.com>
2021-06-09 16:32:46 +0100

    Button Change Background Color When Active
    --
    JD

--- src/Controller/MainScreenController.java
+++ src/Controller/MainScreenController.java
@@ -16,23 +16,38 @@ import javafx.stage.Stage;
 import java.awt.*;
 
 
-public class MainScreenController{
+public class MainScreenController {
 
-    public Button editProfileBtn = new Button();
+    private final String BUTTON_SELECTED = "-fx-background-color: #6bbfa3;";
     private Stage currentStage = null;
     private boolean firstUserRun = false;
+    @FXML
+    private Button controlledAreasButton = new Button();
+    @FXML
+    private Button refrigeratorsButton = new Button();
+    @FXML
+    private Button editProducts = new Button();
+    @FXML
+    private Button editBTN = new Button();
+    @FXML
+    private Button validate = new Button();
+    @FXML
+    private Button allRastData = new Button();
+    @FXML
+    private Button editProfileBtn = new Button();
+    @FXML
+    private Button suppliersButton = new Button();
     @FXML
     private Pane receivedPane;
 
     /**
      * Referencia apresentação de dados: 20/04/2021
      * https://www.youtube.com/watch?v=tw_NXq08NUE
-     *
+     * <p>
      * Referência filtro de pesquisa: 26/04/2021
      * https://www.youtube.com/watch?v=FeTrcNBVWtg
-     *
      */
-    public void start() throws Exception{
+    public void start() throws Exception {
         Stage MainScreen = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("/View/MainScreen.fxml"));
         MainScreen.getIcons().add(new Image("/Images/logoicon.png"));
@@ -43,12 +58,12 @@ public class MainScreenController{
         MainScreen.show();
     }
 
-    public void editProfile(){
+    public void editProfile() {
         alerts(Alert.AlertType.WARNING, "Função Indisponível", "Esta função ainda não está disponível!").showAndWait();
 
     }
 
-    public void logout() throws Exception{
+    public void logout() throws Exception {
         currentStage = (Stage) editProfileBtn.getScene().getWindow();
         currentStage.close();
         Main.u = null;
@@ -56,7 +71,7 @@ public class MainScreenController{
 
     }
 
-    protected static Alert alerts(Alert.AlertType aAlertType, String aTitle, String aText){
+    protected static Alert alerts(Alert.AlertType aAlertType, String aTitle, String aText) {
         Alert generalAlert = new Alert(aAlertType);
         Stage stage = (Stage) generalAlert.getDialogPane().getScene().getWindow();
         stage.getIcons().add(new Image("/Images/logoicon.png"));
@@ -66,95 +81,117 @@ public class MainScreenController{
         return generalAlert;
     }
 
-    public void collapse(){
+    public void collapse() {
         System.out.println("home (collapse) clicked!");
-
-        if(receivedPane != null){
+        resetButtonStyle();
+        if (receivedPane != null) {
             receivedPane.setVisible(false);
         }
     }
 
-    public void showUsersTable(){
+    public void showUsersTable() {
+        resetButtonStyle();
+        editBTN.setStyle(BUTTON_SELECTED);
         try {
             Pane newLoadedPane = FXMLLoader.load(getClass().getResource("/View/AllUsersPane.fxml"));
 
             receivedPane.getChildren().add(newLoadedPane);
             receivedPane.setVisible(true);
 
-        }catch(Exception e ){
+        } catch (Exception e) {
             System.out.println("erro o loader " + e);
             e.printStackTrace();
         }
     }
 
-    @FXML
-    private Button refrigeratorsButton;
-
-    public void showRefrigeratorsTable(){
-        refrigeratorsButton.setStyle("-fx-background-color: #ff0000;");
+    public void showRefrigeratorsTable() {
+        resetButtonStyle();
+        refrigeratorsButton.setStyle(BUTTON_SELECTED);
         try {
             Pane newLoadedPane = FXMLLoader.load(getClass().getResource("/View/RefrigeratorsPane.fxml"));
             receivedPane.getChildren().add(newLoadedPane);
             receivedPane.setVisible(true);
-        }catch(Exception e ){
+        } catch (Exception e) {
             System.out.println("erro o loader " + e);
             e.printStackTrace();
         }
     }
 
-    public void showProductsTable(){
+    public void showProductsTable() {
+        resetButtonStyle();
+        editProducts.setStyle(BUTTON_SELECTED);
         try {
             Pane newLoadedPane = FXMLLoader.load(getClass().getResource("/View/ProductsPane.fxml"));
             receivedPane.getChildren().add(newLoadedPane);
             receivedPane.setVisible(true);
-        }catch(Exception e ){
+        } catch (Exception e) {
             System.out.println("erro o loader " + e);
             e.printStackTrace();
         }
     }
 
-    public void showControlledAreasTable(){
+    public void showControlledAreasTable() {
+        resetButtonStyle();
+        controlledAreasButton.setStyle(BUTTON_SELECTED);
         try {
             Pane newLoadedPane = FXMLLoader.load(getClass().getResource("/View/SectionsPane.fxml"));
             receivedPane.getChildren().add(newLoadedPane);
             receivedPane.setVisible(true);
-        }catch(Exception e ){
+        } catch (Exception e) {
             System.out.println("erro o loader " + e);
             e.printStackTrace();
         }
     }
 
-    public void showFornecedoresTable(){
+    public void showFornecedoresTable() {
+        resetButtonStyle();
+        suppliersButton.setStyle(BUTTON_SELECTED);
         try {
             Pane newLoadedPane = FXMLLoader.load(getClass().getResource("/View/FornecedoresPane.fxml"));
             receivedPane.getChildren().add(newLoadedPane);
             receivedPane.setVisible(true);
-        }catch(Exception e ){
+        } catch (Exception e) {
             System.out.println("erro o loader " + e);
             e.printStackTrace();
         }
     }
 
-    public void showValidationsPane(){
+    public void showValidationsPane() {
+        resetButtonStyle();
+        validate.setStyle(BUTTON_SELECTED);
         try {
             Pane newLoadedPane = FXMLLoader.load(getClass().getResource("/View/ValidatePane.fxml"));
             receivedPane.getChildren().add(newLoadedPane);
             receivedPane.setVisible(true);
-        }catch(Exception e ){
+        } catch (Exception e) {
             System.out.println("erro o loader " + e);
             e.printStackTrace();
         }
     }
 
-    public void showAllRastData(){
+    public void showAllRastData() {
+        resetButtonStyle();
+        allRastData.setStyle(BUTTON_SELECTED);
         try {
             Pane newLoadedPane = FXMLLoader.load(getClass().getResource("/View/allDataPane.fxml"));
             receivedPane.getChildren().add(newLoadedPane);
             receivedPane.setVisible(true);
-        }catch(Exception e ){
+        } catch (Exception e) {
             System.out.println("erro o loader " + e);
             e.printStackTrace();
         }
+    }
+
+    private void resetButtonStyle() {
+        String originalStyle = "-fx-background-color: #253437;";
+        controlledAreasButton.setStyle(originalStyle);
+        refrigeratorsButton.setStyle(originalStyle);
+        editProducts.setStyle(originalStyle);
+        editBTN.setStyle(originalStyle);
+        validate.setStyle(originalStyle);
+        allRastData.setStyle(originalStyle);
+        editProfileBtn.setStyle(originalStyle);
+        suppliersButton.setStyle(originalStyle);
     }
 
 }
