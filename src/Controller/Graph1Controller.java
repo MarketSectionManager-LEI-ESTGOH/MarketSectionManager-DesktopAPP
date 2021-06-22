280b36320c11f1d2d84933f0f737b59139019b61
João Duarte <71460038+jduarte98@users.noreply.github.com>
2021-06-22 16:05:40 +0100

    Graph 1 Fully Functional
    ~JD~

--- src/Controller/Graph1Controller.java
+++ src/Controller/Graph1Controller.java
@@ -6,15 +6,18 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.StackedAreaChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import Model.ConnectDB.*;
 
+import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -24,7 +27,7 @@ import java.util.Random;
 
 public class Graph1Controller {
     @FXML
-    private Pane graph1_pane;
+    private Pane graph1_pane = new Pane();
     @FXML
     private NumberAxis xAxis = new NumberAxis(1,31,1);
     @FXML
@@ -33,15 +36,20 @@ public class Graph1Controller {
     private StackedAreaChart<Number, Number> sac = new StackedAreaChart<>(xAxis, yAxis);
     @FXML
     private ComboBox<String> refrigeratorsDesignationCB;
+    @FXML
+    private Button refreshGraph1BTN;
     private ObservableList<Float> tempsMin = FXCollections.observableArrayList();
     private ObservableList<Float> tempsMax = FXCollections.observableArrayList();
     private Date today = new Date();
     private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-    private int randomStartGraph = -1;
-    private String graphTitle = "";
+    private static int randomStartGraph = -1;
+    private static String graphTitle = "";
+    private XYChart.Series<Number, Number> afternoonTemps = null;
+    private XYChart.Series<Number, Number> morningTemps = null;
 
     @FXML
     public void initialize() {
+        System.out.println("AFTER: ___ randomStartGraph: " + randomStartGraph + " __ graphTitle: " + graphTitle);
         getRefrigeratorsList();
         generateGraph(randomStartGraph, graphTitle);
 
@@ -50,16 +58,20 @@ public class Graph1Controller {
     private void getRefrigeratorsList(){
         ObservableList<String> refrigerators = ConnectDB.getRefrigeratorsIDandDesign();
         refrigeratorsDesignationCB.setItems(refrigerators);
-        int generatedListIndex = new Random().nextInt(refrigerators.size()+1);
-        graphTitle = refrigerators.get(generatedListIndex);
-        randomStartGraph = Integer.parseInt(refrigerators.get(generatedListIndex).split(" - ")[0]);
+        if((graphTitle.equals("")) && (randomStartGraph == -1)){
+            int generatedListIndex = new Random().nextInt(refrigerators.size()+1);
+            graphTitle = refrigerators.get(generatedListIndex);
+            randomStartGraph = Integer.parseInt(refrigerators.get(generatedListIndex).split(" - ")[0]);
+        }
     }
 
     private void generateGraph(int aID, String aGraphTitle){
+        System.out.println("@genrateGraph: aID: " + aID + " _ aGraphTitle: " + aGraphTitle );
         populateTemps(aID);
         sac.setTitle("Variação da Temperatura (ºC) \nArca: " + aGraphTitle);
-        XYChart.Series<Number, Number> morningTemps = new XYChart.Series<>();
         int xMorningCounter = 1, xAfternoonCounter = 1;
+        sac.getData().removeAll(morningTemps,afternoonTemps);
+        XYChart.Series<Number, Number> morningTemps = new XYChart.Series<>();
         morningTemps.setName("Mínimo");
         for(Float tempMinValue : tempsMin) {
             morningTemps.getData().add(new XYChart.Data(xMorningCounter, tempMinValue));
@@ -73,6 +85,8 @@ public class Graph1Controller {
         }
         sac.getData().addAll(morningTemps,afternoonTemps);
         sac.setPrefSize(450,300);
+        randomStartGraph = -1;
+        graphTitle = "";
     }
 
     private void populateTemps(int aID){
@@ -94,6 +108,23 @@ public class Graph1Controller {
                 System.out.println(e);
             }
         }
+    }
+
+    public void refreshGraph(){
+       String selectedRefrigerator [] = refrigeratorsDesignationCB.getSelectionModel().getSelectedItem().split(" - ");
+        randomStartGraph = Integer.parseInt(selectedRefrigerator[0]);
+        graphTitle = selectedRefrigerator[1];
+        Pane newLoadedPane = null;
+        try {
+            newLoadedPane = FXMLLoader.load(getClass().getResource("/View/Graph1.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        graph1_pane.getChildren().clear();
+        graph1_pane.getChildren().add(newLoadedPane);
+        graph1_pane.setVisible(true);
+
+
     }
 
 }
