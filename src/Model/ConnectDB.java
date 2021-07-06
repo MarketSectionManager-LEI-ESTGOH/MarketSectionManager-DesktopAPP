180e96304ba8c116862118c94fdc4d6dafbec721
Bladed <fmr7@live.com>
2021-07-06 18:08:04 +0100

    Commit of something

--- src/Model/ConnectDB.java
+++ src/Model/ConnectDB.java
@@ -1,13 +1,15 @@
 package Model;
 
-import Controller.MainScreenController;
+import com.mysql.jdbc.exceptions.MySQLDataException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Alert;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
-import java.sql.DriverManager;
 import java.util.Properties;
 
 public class ConnectDB {
@@ -845,6 +847,10 @@ public class ConnectDB {
             while(rs.next()){
                 list.add(new Temperatura(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getDate(4), rs.getString(5)));
             }
+        } catch (MySQLDataException e) {
+            System.out.println("!! Class Not Instanciaded !!\n"+e);
+            return null;
+
         } catch (SQLException e) {
             System.out.println("!! SQL Exception !!\n"+e);
             e.printStackTrace();
@@ -860,7 +866,6 @@ public class ConnectDB {
         } catch (InstantiationException e) {
             System.out.println("!! Class Not Instanciaded !!\n"+e);
             return null;
-
         } finally {
             if (conn != null) {
                 try {
